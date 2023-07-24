53c0b288f786cba943e10fed6e85bcfada420920
Leon <leon.karho.chung@gmail.com>
2023-07-24 14:28:47 -0400

    add variant and disabled props to type definition - style accordingly to variant and pseudo class styles

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -3,10 +3,27 @@ import styles from "./Button.module.scss";
 
 interface Props {
 	children: ReactNode;
+	variant?: "default" | "secondary";
+	disabled?: boolean;
 }
 
-const Button: FunctionComponent<Props> = ({ children }) => {
-	return <button className={styles.button_primary}>{children}</button>;
+const Button: FunctionComponent<Props> = ({
+	children,
+	variant = "default",
+	disabled,
+}) => {
+	return (
+		<button
+			disabled={disabled}
+			className={
+				variant === "secondary"
+					? styles.button_secondary
+					: styles.button_primary
+			}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
