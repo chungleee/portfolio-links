83665b9d0d1dfb7eb36ae6a3a820d8708043b22f
Leon <leon.karho.chung@gmail.com>
2023-07-25 13:51:06 -0400

    custom RHF text field that accepts register and input name as props - properly typed

--- src/components/common/TextField/TextField.tsx
+++ src/components/common/TextField/TextField.tsx
@@ -1,16 +1,23 @@
 import { FunctionComponent } from "react";
+import { UseFormRegister, FieldPath } from "react-hook-form";
 import { FiLink } from "react-icons/fi";
 import styles from "./TextField.module.scss";
+import { Inputs } from "@/app/page";
 
 interface TextfieldProps {
 	defaultValue?: string;
+	register: UseFormRegister<Inputs>;
+	inputName: FieldPath<Inputs>;
 }
 
-const TextField: FunctionComponent<TextfieldProps> = (props) => {
+const TextField: FunctionComponent<TextfieldProps> = ({
+	register,
+	inputName,
+}) => {
 	return (
 		<div className={styles.textfield}>
 			<FiLink className={styles.textfield_icon} />
-			<input {...props} type='text' />
+			<input {...register(inputName)} type='text' />
 		</div>
 	);
 };
