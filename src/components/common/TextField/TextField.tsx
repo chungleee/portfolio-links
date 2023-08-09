ca066c2cfe3ea40773df9fa9f7388546931d2770
Leon <leon.karho.chung@gmail.com>
2023-08-09 15:52:19 -0400

    add div container - accept classname

--- src/components/common/TextField/TextField.tsx
+++ src/components/common/TextField/TextField.tsx
@@ -10,26 +10,31 @@ interface ITextfieldProps {
 	placeholder?: string;
 	iconVariant: "link" | "mail" | "lock";
 	type?: "text" | string;
+	className?: string;
 }
 
 const TextField = forwardRef<HTMLInputElement, ITextfieldProps>(
-	({ error, iconVariant, type, placeholder, label, ...props }, ref) => {
-		console.log("error: ", error);
+	(
+		{ error, iconVariant, type, placeholder, label, className, ...props },
+		ref
+	) => {
 		return (
-			<label className={styles.label}>
-				{label}
-				<div
-					className={
-						error ? `${styles.textfield} ${styles.error}` : styles.textfield
-					}
-				>
-					<Icon variant={iconVariant} className={styles.textfield_icon} />
-					<input ref={ref} {...props} type={type} placeholder={placeholder} />
-					{error?.message && (
-						<small className={styles.error_message}>{error.message}</small>
-					)}
-				</div>
-			</label>
+			<div className={className}>
+				<label className={styles.label}>
+					{label}
+					<div
+						className={
+							error ? `${styles.textfield} ${styles.error}` : styles.textfield
+						}
+					>
+						<Icon variant={iconVariant} className={styles.textfield_icon} />
+						<input ref={ref} {...props} type={type} placeholder={placeholder} />
+						{error?.message && (
+							<small className={styles.error_message}>{error.message}</small>
+						)}
+					</div>
+				</label>
+			</div>
 		);
 	}
 );
