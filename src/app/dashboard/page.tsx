af2b55169c4c6c11297d7d11c6c9b1ab0400cf0d
Leon <leon.karho.chung@gmail.com>
2023-08-15 16:06:25 -0400

    import usefieldarray hook - initial setup - append input fields when button clicked

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import Button from "@/components/common/Button/Button";
 import styles from "./page.module.scss";
 import Image from "next/image";
 import ipadIllustration from "../../../public/images/illustration-empty.svg";
+import { useForm, useFieldArray } from "react-hook-form";
+import TextField from "@/components/common/TextField/TextField";
 
 const Dashboard = () => {
+	const { control, register } = useForm();
+	const { fields, append, remove } = useFieldArray({
+		control,
+		name: "foliolinks",
+	});
 	return (
 		<div className={styles.dashboard}>
 			<section>
@@ -12,11 +20,30 @@ const Dashboard = () => {
 					world!
 				</p>
 				<h2>Customize your links</h2>
-				<Button variant='secondary'>+ Add new link</Button>
+				<Button
+					onClick={() => {
+						append({ projectName: "", projectLink: "" });
+					}}
+					variant='secondary'
+				>
+					+ Add new link
+				</Button>
 			</section>
 
 			<section className={styles.dashboard_create__container}>
 				<div className={styles.dashboard_create__container_add__link}>
+					{/* add link cards go here */}
+					{fields &&
+						fields.map((field, index) => {
+							return (
+								<TextField
+									key={field.id}
+									label='Project name'
+									iconVariant='link'
+									{...register(`foliolinks.${index}.value`)}
+								/>
+							);
+						})}
 					<div>
 						<Image
 							src={ipadIllustration}
