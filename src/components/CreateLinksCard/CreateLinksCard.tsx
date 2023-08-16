0d795cd7eba78e5406dba1b5a33ab77279aae0cd
Leon <leon.karho.chung@gmail.com>
2023-08-16 16:25:30 -0400

    create link cards skeleton and styling

--- src/components/CreateLinksCard/CreateLinksCard.tsx
+++ src/components/CreateLinksCard/CreateLinksCard.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./CreateLinksCard.module.scss";
+import TextField from "../common/TextField/TextField";
 
 const CreateLinksCard = () => {
-	return <h1>Create Links Card component</h1>;
+	return (
+		<div className={styles.create_links_card}>
+			<div>
+				<span>link #1</span>
+				<button>remove</button>
+			</div>
+			<TextField
+				label='Project name'
+				iconVariant='zap'
+				placeholder='Enter the name of your project'
+				inputContainerClassName={styles.create_links_card__textfields}
+			/>
+			<TextField
+				label='Link'
+				iconVariant='link'
+				placeholder='e.g. https//www.github.com/project'
+				inputContainerClassName={styles.create_links_card__textfields}
+			/>
+		</div>
+	);
 };
 
 export default CreateLinksCard;
