eac28c212bddeb98a077b2c80adf28df3790fc4a
Leon <leon.karho.chung@gmail.com>
2023-08-21 15:38:23 -0400

    profile page intro - personal details form

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,5 +1,23 @@
+import TextField from "@/components/common/TextField/TextField";
+import styles from "./page.module.scss";
+
 const Profile = () => {
-	return <h1>this is the profile page</h1>;
+	return (
+		<div className={styles.profile}>
+			<section className={styles.profile__intro}>
+				<p>Add your details to create a personal touch to your profile.</p>
+				<h2>Profile Details</h2>
+			</section>
+
+			<form className={styles.profile__form_section}>
+				<div className={styles.profile__form_section__personal_deets}>
+					<TextField label='First name' type='text' />
+					<TextField label='Last name' type='text' />
+					<TextField label='Email' type='email' />
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Profile;
