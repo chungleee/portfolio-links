aaae79fd39a04be763437fda3004bc105e7efbde
Leon <leon.karho.chung@gmail.com>
2023-08-21 16:08:52 -0400

    image upload section - custom file input

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,5 +1,6 @@
 import TextField from "@/components/common/TextField/TextField";
 import styles from "./page.module.scss";
+import Icon from "@/components/common/Icons/Icon";
 
 const Profile = () => {
 	return (
@@ -10,6 +11,16 @@ const Profile = () => {
 			</section>
 
 			<form className={styles.profile__form_section}>
+				<div className={styles.profile__form_section__image_upload}>
+					<p>Profile picture</p>
+					<label>
+						<Icon variant='image' />
+						+ Upload Image
+						<input type='file' />
+					</label>
+					<small>Image must be below 5MB. Use WebP, PNG or JPG formats.</small>
+				</div>
+
 				<div className={styles.profile__form_section__personal_deets}>
 					<TextField label='First name' type='text' />
 					<TextField label='Last name' type='text' />
