b4fca508a76a8e0cc5888c36a9331c3979776e16
Leon <leon.karho.chung@gmail.com>
2023-08-22 16:59:18 -0400

    profile form schema validation - profile pic refinements

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,9 +1,58 @@
+"use client";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 import TextField from "@/components/common/TextField/TextField";
 import styles from "./page.module.scss";
 import Icon from "@/components/common/Icons/Icon";
 import Button from "@/components/common/Button/Button";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const ACCEPTED_FORMATS = [
+	"application/x-photoshop",
+	"image/jpg",
+	"image/jpeg",
+	"image/webp",
+	"image/png",
+];
+
+const MAX_FILE_SIZE = 5000000;
+
+const profileSchema = z.object({
+	profilePic: z
+		.any()
+		.refine((files: FileList) => {
+			return files.length;
+		}, "Please upload an image")
+		.refine((files: FileList) => {
+			return ACCEPTED_FORMATS.includes(files[0]?.type);
+		}, "This format is not accepted")
+		.refine((files: FileList) => {
+			return files[0]?.size <= MAX_FILE_SIZE;
+		}, "File needs to be 5MB or less"),
+	firstName: z.string(),
+	lastName: z.string(),
+	email: z.string(),
+});
+
+type TProfileValues = z.infer<typeof profileSchema>;
 
 const Profile = () => {
+	const {
+		handleSubmit,
+		register,
+		watch,
+		formState: { errors },
+	} = useForm<TProfileValues>({
+		resolver: zodResolver(profileSchema),
+	});
+
+	const handleSave = (data: TProfileValues) => {
+		console.log("form data: ", data);
+	};
+
+	if (errors) console.log("errors: ", errors);
+	console.log("profile pic: ", watch("profilePic"));
+
 	return (
 		<div className={styles.profile}>
 			<section className={styles.profile__intro}>
@@ -18,7 +67,7 @@ const Profile = () => {
 						<label>
 							<Icon variant='image' />
 							+ Upload Image
-							<input type='file' />
+							<input type='file' {...register("profilePic")} />
 						</label>
 						<small>
 							Image must be below 5MB. Use WebP, PNG or JPG formats.
@@ -32,23 +81,30 @@ const Profile = () => {
 						inputContainerClassName={styles.textfields}
 						label='First name'
 						type='text'
+						{...register("firstName")}
 					/>
 					<TextField
 						labelClassName={styles.textfields_layout}
 						inputContainerClassName={styles.textfields}
 						label='Last name'
 						type='text'
+						{...register("lastName")}
 					/>
 					<TextField
 						labelClassName={styles.textfields_layout}
 						inputContainerClassName={styles.textfields}
 						label='Email'
 						type='email'
+						{...register("email")}
 					/>
 				</div>
 
 				<section className={styles.save_button}>
-					<Button variant='default' type='submit'>
+					<Button
+						onClick={handleSubmit(handleSave)}
+						variant='default'
+						type='submit'
+					>
 						Save
 					</Button>
 				</section>
