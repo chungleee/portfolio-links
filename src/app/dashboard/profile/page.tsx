91daaa62f77f6e8dbd0a8c917b4dc6167a1b4d64
Leon <leon.karho.chung@gmail.com>
2023-08-25 16:33:40 -0400

    upload image to local state and preview the image blob

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { ImgHTMLAttributes, useState } from "react";
 import { useForm } from "react-hook-form";
 import TextField from "@/components/common/TextField/TextField";
 import styles from "./page.module.scss";
@@ -6,8 +7,11 @@ import Icon from "@/components/common/Icons/Icon";
 import Button from "@/components/common/Button/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TProfileFormValues, profileSchema } from "../model";
+import Image from "next/image";
 
 const Profile = () => {
+	const [previewImg, setPreviewImg] = useState<string>("");
+
 	const {
 		handleSubmit,
 		register,
@@ -21,6 +25,7 @@ const Profile = () => {
 		console.log("form data: ", data);
 	};
 
+	console.log("image preview: ", previewImg);
 	console.log("errors: ", errors);
 
 	return (
@@ -39,10 +44,23 @@ const Profile = () => {
 							+ Upload Image
 							<input
 								type='file'
-								{...register("profilePic")}
+								{...register("profilePic", {
+									onChange: (event) => {
+										const file = event.target.files[0];
+										setPreviewImg(URL.createObjectURL(file));
+									},
+								})}
 								accept='image/jpg, image/jpeg, image/webp, image/png'
 							/>
 						</label>
+						{previewImg ? (
+							<Image
+								src={previewImg}
+								alt='Preview of uploaded profile picture'
+								width={150}
+								height={150}
+							/>
+						) : null}
 						<small>
 							Image must be below 5MB. Use WebP, PNG or JPG formats.
 						</small>
