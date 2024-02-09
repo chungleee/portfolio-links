e427c6c98489093a2c06f1f43e6a3a43d2c16b18
Leon <leon.karho.chung@gmail.com>
2024-02-09 14:18:19 -0500

    store jwt to localStorage and redirect to /dashboard on success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import { redirect } from "next/navigation";
+"use client";
+import { redirect, useRouter } from "next/navigation";
+import useAuth from "../hooks/useAuth";
+import { useEffect } from "react";
 
 const Home = () => {
-	redirect("/login");
+	/**
+	 * 1. check localStorage to see if user is logged in
+	 * 2. if logged in -> redirect to /dashboard
+	 * 3. else -> redirect to /login
+	 */
+	const router = useRouter();
+	const isAuth = useAuth();
+
+	useEffect(() => {
+		if (!isAuth) {
+			router.push("/login");
+		} else {
+			router.push("/dashboard");
+		}
+
+		console.log("home");
+	}, [isAuth]);
 };
 
 export default Home;
