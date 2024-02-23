6d2f7fcd718913471d0fccf4cbca8c64eab1e52d
Leon <leon.karho.chung@gmail.com>
2024-02-23 14:36:35 -0500

    test env vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
 "use client";
-import { redirect, useRouter } from "next/navigation";
-import useAuth from "../hooks/useAuth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+	const [ping, setPing] = useState();
+
+	useEffect(() => {
+		const pingServer = async () => {
+			const url = process.env.NEXT_PUBLIC_SERVER;
+			const res = await fetch(`${url}/ping`);
+			const json = await res.json();
+
+			setPing(json.ping);
+		};
+
+		pingServer();
+	}, []);
 	/**
 	 * 1. check localStorage to see if user is logged in
 	 * 2. if logged in -> redirect to /dashboard
 	 * 3. else -> redirect to /login
 	 */
-	const router = useRouter();
-	const isAuth = useAuth();
-
-	useEffect(() => {
-		if (!isAuth) {
-			router.push("/login");
-		} else {
-			router.push("/dashboard");
-		}
-		console.log("home");
-	}, [isAuth]);
+	return <div>{ping}</div>;
 };
 
 export default Home;
