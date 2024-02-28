21a6a1b04e6cd529eba116d84e916b5579aebcf2
Leon <leon.karho.chung@gmail.com>
2024-02-28 14:53:28 -0500

    middleware to check if cookie is present, if not: redirect to /login, else, redirect to /dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,14 @@ import type { NextRequest } from "next/server";
 
 export const middleware = (request: NextRequest) => {
 	let authCookie = request.cookies.get("foliolinks_auth");
-
+	console.log("authCookie: ", authCookie);
 	if (!authCookie) {
 		return NextResponse.redirect(new URL("/login", request.url));
+	} else {
+		return NextResponse.redirect(new URL("/dashboard", request.url));
 	}
 };
 
 export const config = {
-	matcher: "/dashboard",
+	matcher: ["/", "/dashboard/:path*"],
 };
